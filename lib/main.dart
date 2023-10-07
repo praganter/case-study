2f3c4c794cd905ca6c4b983d8bddb9a22aabbd14
Batuhan Yetgin <byetgin@engineer.com>
2023-10-07 21:56:22 +0300

    provider scope added to main. LETS BEGIN !

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:case_study/core/utils/custom_router.dart';
 import 'package:case_study/product/constat/page_routes_constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -11,6 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: PageRoutesConstant.routes,
       navigatorKey: CustomRouter.navigatorKey,
