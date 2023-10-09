d7b24d7085dca56147d6c3831989f11098fd7b47
Batuhan Yetgin <batuhan.yetgin@xinerji.com.tr>
2023-10-09 04:44:02 +0300

    new strings and page route added

--- lib/product/constat/page_routes_constants.dart
+++ lib/product/constat/page_routes_constants.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 
 import '../../feature/home/home_screen.dart';
+import '../../feature/home/searchDetail/search_detail_screen.dart';
 import '../../feature/program/program_screen.dart';
 
 class PageRoutesConstant {
   final BuildContext context;
   PageRoutesConstant({required this.context});
   static Map<String, Widget Function(BuildContext)> routes = {
-    '/': (context) => const HomeScreen(),
-    '/program': (context) => const ProgramScreen(),
+    homeScreenPath: (context) => const HomeScreen(),
+    programPath: (context) => const ProgramScreen(),
+    searchdetailsPath: (context) => const SearchDetailScreen(),
   };
 
   static String get homeScreenPath => '/';
   static String get programPath => '/program';
+  static String get searchdetailsPath => '/searchDetails';
 }
--- lib/product/constat/string_constants.dart
+++ lib/product/constat/string_constants.dart
@@ -4,4 +4,11 @@ class StringConstants {
   static const String apiKey = "API_KEY";
   static const String baseUrl = "BASE_URL";
   static const String resultsFound = " Results Found";
+  static const String myProgramsBox = "myPorgramsBox";
+  static const String selectProgram = "Select Program";
+
+  //! Search
+  static const String searchDetails = "Search Details";
+  static const String selectMuscle = "Select Muscle";
+  static const String selectType = "Select Type";
 }
