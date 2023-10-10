37064617d8696a47fee998debbf8912497276e56
Batuhan Yetgin <batuhan.yetgin@xinerji.com.tr>
2023-10-10 12:32:32 +0300

    new string constants added

--- lib/product/constat/string_constants.dart
+++ lib/product/constat/string_constants.dart
@@ -1,14 +1,25 @@
 class StringConstants {
   static const String home = "Home";
   static const String program = "Program";
+  static const String programs = "Programs";
   static const String apiKey = "API_KEY";
   static const String baseUrl = "BASE_URL";
   static const String resultsFound = " Results Found";
-  static const String myProgramsBox = "myPorgramsBox";
   static const String selectProgram = "Select Program";
+  static const String nameOfNewProgram = "Name of new program";
+  static const String addNew = "Add New";
+  static const String selectedProgram = "Selected Program: ";
+  static const String name = "Name : ";
+  static const String type = "Type : ";
+  static const String muscle = "Muscle : ";
+  static const String exerciseAdapter = "ExerciseAdapter";
 
   //! Search
   static const String searchDetails = "Search Details";
   static const String selectMuscle = "Select Muscle";
   static const String selectType = "Select Type";
+
+  //! Hive
+  static const String programBox = "programBox";
+  static const String exerciseBox = "exerciseBox";
 }
